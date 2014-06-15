e5e92bae8dd5702f7d8eb6d205be3315637d6ca8
Xiang Wu <xiangw.ahu@gmail.com>
2014-06-15 18:29:14 +0800

    transform json into table, only "submission" api surported.

---
+++ try/format.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+# codeforces hitomi project
+#
+# wx2486, 2014-6-15
+#
+# try with python
+#
+# functions:
+# read json from json.dat
+# transform it into table format
+# save it into table.dat
+
+print('Eyes are open!')
+
+import json
+
+print('Eating hard disk...')
+
+json_file = open('json.dat', 'r')
+
+print('Done.')
+print('Loading json...')
+
+json_target = json.load(json_file)
+
+print('Done.')
+
+subms = json_target['result']
+
+print('Focus on content.')
+
+fields = ('id',
+          'contestId',
+          'creationTimeSeconds',
+          'relativeTimeSeconds',
+          'problem',
+          'author',
+          'programmingLanguage',
+          'verdict',
+          'testset',
+          'passedTestCount',
+          'timeConsumedMillis',
+          'memoryConsumedBytes')
+
+table = ''
+for i in range(len(fields)):
+    if i > 0:
+        table += '\t'
+    table += fields[i]
+table += '\n'
+
+print('Transform json into a table...')
+
+for sub in subms:
+    for i in range(len(fields)):
+        if len(sub['author']['members']) != 1:  # ignore teams
+            continue
+        if i > 0:
+            table += '\t'
+        if fields[i] == 'problem':
+            table += sub['problem']['index']
+        elif fields[i] == 'author':
+            table += sub['author']['members'][0]['handle']
+        else:
+            table += str(sub[fields[i]])
+    table += '\n'
+
+print('Done.')
+print('Dump table...')
+
+table_file = open('table.dat', 'w')
+table_file.write(table)
+table_file.close()
+
+print('Done.')
+print('Eyes closed.')
