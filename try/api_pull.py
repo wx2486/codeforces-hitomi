408ba6c4b7a7dc1b962fce0bd28908c9d7099c95
Xiang Wu <xiangw.ahu@gmail.com>
2014-06-15 18:45:22 +0800

    you can disable some config entries now

--- try/api_pull.py
+++ try/api_pull.py
@@ -19,13 +19,13 @@ import json
 # edit this block to configure this script
 
 # Id of the contest. It is not the round number. It can be seen in contest URL. For example: /contest/374/status
-url_contestId = '374'
+url_contestId = 1
 
-# 1-based index of the first submission to return.
-url_from = '1'
+# 1-based index of the first submission to return. Ignored if less than 1
+url_from = 0
 
-# Number of returned submissions.
-url_count = '10'
+# Number of returned submissions. ignored if less than 1
+url_count = 0
 
 # Should I print data on the screen? True or False
 print_target = False
@@ -35,7 +35,12 @@ print_target = False
 
 print('Mission confirmed.')
 
-target_url = 'http://codeforces.com/api/contest.status?contestId=' + url_contestId + '&from=' + url_from + '&count=' + url_count
+target_url = 'http://codeforces.com/api/contest.status?contestId='\
+    + str(url_contestId)
+if url_from > 0:
+    target_url += '&from=' + str(url_from)
+if url_count > 0:
+    target_url += '&count=' + str(url_count)
 
 print('Link start.')
 print('Target url: ' + target_url)
@@ -44,21 +49,35 @@ print('Downloading...')
 target = urllib.request.urlopen(target_url)
 
 print('Target obtained.')
+print('Loading target...')
+
+json_target = json.loads(target.read().decode())
+
+print('Done.')
+print('Checking server response status...')
+
+if json_target['status'] != 'OK':
+    print('Status: ' + json_target['status'])
+    print('Comment: ' + json_target['comment'])
+    print('Abort.')
+    exit()
+
+print('Status: OK')
 print('Transform target into nice json format...')
 
-json_target = json.dumps(json.loads(target.read().decode()),sort_keys=True, indent=4)
+json_target_str = json.dumps(json_target, sort_keys=True, indent=4)
 
 print('Done.')
 
 if (print_target):
     print('Target:')
-    print(json_target)
+    print(json_target_str)
 
 print('Dumping target in nice json format...')
 
-raw_file = open('json.dat', 'w')
-raw_file.write(json_target)
-raw_file.close()
+json_file = open('json.dat', 'w')
+json_file.write(json_target_str)
+json_file.close()
 
 print('Done.')
 print('Eyes closed.')
